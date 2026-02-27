a01c31b146a4f36bc54983f3588596cf24d617b0
durinwinter <caleb@winterwinds.io>
2026-02-27 12:29:31 -0700

    removed mars-sim Java nonsense

--- mars-sim-3.10 (1).0/mars-sim-core/src/main/java/com/mars_sim/core/resourceprocess/ResourceProcessAssessment.java
+++
@@ -1,9 +0,0 @@
-package com.mars_sim.core.resourceprocess;
-
-import java.io.Serializable;
-
-public record ResourceProcessAssessment(double inputScore, double outputScore, double overallScore,
-                        boolean inputsAvailable)
-        implements Serializable {
-
-}
